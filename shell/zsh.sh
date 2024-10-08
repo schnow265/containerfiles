a284f7dd56345424ae8701e498cee1b04063d35c
schnow265 <thesnowbox@icloud.com>
2024-10-08 12:49:19 +0200

    remove zinit

--- shell/zsh.sh
+++ shell/zsh.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
-
 printf "\n\nalias cls=clear\nalias ll='lsd -Al'\nalias ls=lsd\n\n\neval '$(starship init zsh)'" >> ~/.zshrc
