b937bb429b0e384a24489b545635e45bb54203e9
schnow265 <thesnowbox@icloud.com>
2024-10-08 12:07:47 +0200

    add starship config and fix zsh script

--- shell/zsh.sh
+++ shell/zsh.sh
@@ -2,4 +2,4 @@
 
 bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
 
-printf "\n\nalias cls=clear\nalias ll='lsd -Al'\nalias ls=lsd" >>
+printf "\n\nalias cls=clear\nalias ll='lsd -Al'\nalias ls=lsd\n\n\neval '$(starship init zsh)'" >> ~/.zshrc
