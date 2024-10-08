31ae9b12b89f00d1d0feb081c7baaf8693f5458c
schnow265 <thesnowbox@icloud.com>
2024-10-08 13:26:14 +0200

    added delta

--- tools/setup.sh
+++ tools/setup.sh
@@ -1,4 +1,9 @@
 cd tools
+
 chmod +x ./tmux.sh
 ./tmux.sh
+
+cd /tmp
+curl "https://github.com/dandavison/delta/releases/download/0.18.2/git-delta_0.18.2_amd64.deb" -O
+sudo dpkg -i git-delta_0.18.2_amd64.deb -y
 cd -
